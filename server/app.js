b8d497ec92c1b21ee620cfc5115ee4c202c66b3d
Bethina Sowmya Sri Durga <sowmyabethina@gmail.com>
2025-06-27 16:55:11 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -5,17 +5,17 @@ const authRoutes = require('./routes/auth');
 const appointmentRoutes = require('./routes/appointments');
 const doctorRoutes = require('./routes/doctors');
 const path = require('path');
+require('dotenv').config();
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-mongoose.connect('mongodb+srv://sowmyabethina:sowmya2004@cluster0.ukr3qhw.mongodb.net/docspot?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/appointments', appointmentRoutes);
